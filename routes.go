35472be21297e4eab382ec8375fe2c9175c9c9ff
Mes Morts <titouanescorneboueu@gmail.com>
2021-09-14 14:04:39 +0200

    Initial commit

--- routes.go
+++ routes.go
@@ -6,8 +6,8 @@ package main
 
 // ! Créer la func pour le chemin de nos routes
 func (s *server) routes() {
-	s.router.HandleFunc("/", s.handleIndex()).Methods("GET")
-	s.router.HandleFunc("/api/movies/{id:[0-9]+}", s.handleMovieDetail()).Methods("GET")
-	s.router.HandleFunc("/api/movies/", s.handleMovieList()).Methods("GET")
-	s.router.HandleFunc("/api/movies/", s.handleMovieCreate()).Methods("POST")
+	s.router.HandleFunc("/", s.handleIndex()).Methods("GET")                             // Afficher dans l'index
+	s.router.HandleFunc("/api/movies/{id:[0-9]+}", s.handleMovieDetail()).Methods("GET") // Affiche un seul film grâce à sont index
+	s.router.HandleFunc("/api/movies/", s.handleMovieList()).Methods("GET")              // Affiche tout les films
+	s.router.HandleFunc("/api/movies/", s.handleMovieCreate()).Methods("POST")           // Créer un film
 }
